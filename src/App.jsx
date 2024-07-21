b6780dafe78c82c8ceafb8646b6d9d6479136668
Ravi22110219 <ravi.kumawat@iitgn.ac.in>
2024-07-21 12:10:40 +0530

    uptodate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import UpperNav from './Components/NavbarSection/UpperNav';
 import NavBar from './Components/NavbarSection/NavBar';
-import './App.css'; // Global styles
 import Slider from './Components/SliderSection/Slider';
 import CourseAndFees from './Components/CourseCards/CourseAndFees';
 import NewsAndNotification from './Components/NewsAndNotificationSection/NewsAndNotification';
@@ -12,47 +12,55 @@ import TestimonialsSection from './Components/TestimonialsSection/TestimonialsSe
 import LocationSection from './Components/LocationSection/LocationSection';
 import AppSection from './Components/AppSection/AppSection';
 import FooterSection from './Components/Footer/FooterSection';
-import BlogPage from './Pages/Blog/BlogPage';
-import GalleryHome from './Pages/Gallery/GalleyHome';
-import PhotoGallery from './Pages/Gallery/PhotoGallery';
-import AdmissionForm from './Pages/Admission/AdmissionForm';
 import CoursesAndBatches from './Pages/CoursesAndBatchesPage/CoursesAndBatches';
-import FeeRefundRules from './Pages/FeeAndScholarship/FeeRefundRules';
+import CoursesAndBatchesNEET from './Pages/CoursesAndBatchesPage/CoursesAndBatchesNEET';
+import AdmissionForm from './Pages/Admission/AdmissionForm';
 import FeeStructure from './Pages/FeeAndScholarship/FeeStructure';
 import Scholarship from './Pages/FeeAndScholarship/Scholarship';
+import FeeRefundRules from './Pages/FeeAndScholarship/FeeRefundRules';
 import ResultsHomePage from './Pages/Results/ResultsHomePege';
+import BlogHomePage from './Pages/Blog/BlogHomePage';
+import PhotoGallery from './Pages/Gallery/PhotoGallery';
+import WhyChooseUs from './Pages/WhyChoiceUs/WhyChooseUs';
+import DirectorsAndteachers from './Pages/AboutUs/DirectorsAndteachers';
+import Home from './Pages/Home/Home'; // Ensure Home is imported
+import Layout from './Layout.jsx'; // Ensure Layout is imported
 import ResultMainPage from './Pages/Results/ResultMainPage';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "course&batch", element: <CoursesAndBatches /> },
+      { path: "course&batchneet", element: <CoursesAndBatchesNEET /> },
+      { path: "admission", element: <AdmissionForm /> },
+      {
+        path: "feeAndScholarship",
+        children: [
+          { path: "feeStructure", element: <FeeStructure /> },
+          { path: "scholarship", element: <Scholarship /> },
+          { path: "feeRefundRules", element: <FeeRefundRules /> },
+        ],
+      },
+      { path: "result", element: <ResultsHomePage /> },
+      { path: "bloghomepage", element: <BlogHomePage />},
+      { path: "gallery", element: <PhotoGallery /> },
+      { path: "whychoiceus", element: <WhyChooseUs />},
+      { path: "directorsandteachers", element: <DirectorsAndteachers /> },
+    ],
+  }
+]);
+
 function App() {
-    return (
-        <div className="App">
-         
-            <UpperNav />
-            <NavBar /> 
-            <Slider/>
-            <CourseAndFees/>
-            <NewsAndNotification/>
-            <AboutSection />
-            <FactsAndEnrollSections />
-            <BlogSection />
-            <TestimonialsSection />
-            <LocationSection />
-            <AppSection />
-            <FooterSection />
-          
-             {/* <GalleryHome /> */}
-            {/* <PhotoGallery /> */}
-             {/* <BlogPage /> */}
-             {/* <AdmissionForm /> */}
-             {/* <CoursesAndBatches /> */}
-             {/* <FeeRefundRules/> */}
-             {/* <FeeStructure /> */}
-             {/* <Scholarship /> */}
-             {/* <ResultsHomePage /> */}
-             {/* <ResultMainPage /> */}
-            {/* Other sections go here */}
-        </div>
-    );
+  return (
+    <div className="App">
+
+      {/* Routing setup */}
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,56 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './Layout.jsx'
-import CoursesAndBatches from './Pages/CoursesAndBatchesPage/CoursesAndBatches.jsx'
-import Home from './Pages/Home/Home.jsx'
-import AdmissionForm from './Pages/Admission/AdmissionForm.jsx'
-import FeeStructure from './Pages/FeeAndScholarship/FeeStructure.jsx'
-import ResultsHomePage from './Pages/Results/ResultsHomePege.jsx'
-import BlogPage from './Pages/Blog/BlogPage.jsx'
-import PhotoGallery from './Pages/Gallery/PhotoGallery.jsx'
-import AdminBlogPage from './Pages/AdminPage/AdminBlogPage.jsx'
-import Scholarship from './Pages/FeeAndScholarship/Scholarship.jsx'
-import FeeRefundRules from './Pages/FeeAndScholarship/FeeRefundRules.jsx'
-import WhyChooseUs from './Pages/WhyChoiceUs/WhyChooseUs.jsx'
-import DirectorsAndteachers from './Pages/AboutUs/DirectorsAndteachers.jsx'
-import BlogHomePage from './Pages/Blog/BlogHomePage.jsx'
-import CoursesAndBatchesNEET from './Pages/CoursesAndBatchesPage/CoursesAndBatchesNEET.jsx'
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout />,
-        children: [
-            { path: "", element: <Home /> },
-            { path: "course&batch", element: <CoursesAndBatches /> },
-            { path: "course&batchneet", element: <CoursesAndBatchesNEET /> },
-            { path: "admission", element: <AdmissionForm /> },
-            {
-                path: "feeAndScholarship",
-                children: [
-                    { path: "feeStructure", element: <FeeStructure /> },
-                    { path: "scholarship", element: <Scholarship /> },
-                    { path: "feeRefundRules", element: <FeeRefundRules /> }, // Correct component
-                ],
-            },
-            { path: "result", element: <ResultsHomePage /> },
-            { path: "bloghomepage", element: <BlogHomePage />},
-            { path: "gallery", element: <PhotoGallery /> },
-            { path: "whychoiceus", element: <WhyChooseUs />},
-            { path: "directorsandteachers", element: <DirectorsAndteachers /> },
-        ],
-    },
-
-     
-])
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- <React.StrictMode>
-    <RouterProvider router={router}/>
+  <React.StrictMode>
+    <App />
   </React.StrictMode>
-)
-
+);
